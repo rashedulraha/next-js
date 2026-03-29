251e4ccd6e5631bb6ca01c246ee317b7612d337e
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-29 19:48:32 +0600

    update project

--- src/app/api/route.js
+++
@@ -1,9 +0,0 @@
-export const feedBack = [
-  { id: 1, message: "good food" },
-  { id: 1, message: "improve" },
-  { id: 1, message: "Oke" },
-];
-
-export async function GET(request) {
-  return Response.json({ status: 200, message: "api created" });
-}
